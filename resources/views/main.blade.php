c2391ceb4db01d0b4c33ea41fbead35e4ef53249
nayaragd <nanypower1215@gmail.com>
2019-10-01 11:58:17 -0300

    Up Projeto

---
+++ resources/views/main.blade.php
@@ -0,0 +1,8 @@
+@extends('principal')
+
+<h2> Corpo do Sistema </h2>
+
+@section('cabecalho')
+<img src="{{url('img/logo.png')}}"/>
+
+
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+
+        <h2>Blade PHP</h2>
+
+    </body>
+</html>
